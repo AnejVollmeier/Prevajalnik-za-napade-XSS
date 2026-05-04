6dd0e02d2f087636572f0b0b52a5a0cdc8130ca5
AnejVollmeier <anej.vollmeier@student.um.si>
2026-05-04 17:18:25 +0200

    Dodana Prisma shema in začetna migracija baze

---
+++ src/backend/prisma/migrations/20260504144640_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Analysis" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "target" TEXT NOT NULL,
+    "inputMode" TEXT NOT NULL,
+    "scoreOverall" INTEGER NOT NULL,
+    "highCount" INTEGER NOT NULL,
+    "mediumCount" INTEGER NOT NULL,
+    "lowCount" INTEGER NOT NULL,
+    "reportJson" TEXT NOT NULL,
+    CONSTRAINT "Analysis_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Analysis_userId_idx" ON "Analysis"("userId");
